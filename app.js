2df5aa893435d90a3ca2da4a13417735bf164532
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-04-29 21:21:07 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express');
+const sql = require("msnodesqlv8");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectionString = "server=ASUSDT;Database=Calendar;Trusted_Connection=Yes;Driver={SQL Server}";
+const query = "SELECT * FROM dbo.Events";
+
+app.get('/events', (req, res) => {
+    sql.query(connectionString, query, (err, rows) => {
+        if (err) {
+            console.error("Error executing query:", err);
+            res.status(500).send("Internal Server Error");
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+const polku = path.join(__dirname, './public')
+app.use(express.static(polku))
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+
